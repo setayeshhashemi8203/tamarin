4e073e23ff6a90c4751dddbc4fa7b58c395c4839
setayeshhashemi8203 <setayeshhashemi8203@gmail.com>
2025-04-05 00:19:24 +0330

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,38 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace tasaod_hesabi
+namespace ekhtelafe_sen
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int a1, a2, a3, s1, s2;
-            a1 = int.Parse(Console.ReadLine());
-            a2 = int.Parse(Console.ReadLine());
-            a3 = int.Parse(Console.ReadLine());
-            s1 = a3 - a2;
-            s2 = a2 - a1;
-            if (s1 == s2)
-                Console.WriteLine("yes");
+
+            int y1, m1, d1, y2, m2, d2, r, r1, r2, r3;
+            y1 = int.Parse(Console.ReadLine());
+            m1 = int.Parse(Console.ReadLine());
+            d1 = int.Parse(Console.ReadLine());
+            y2 = int.Parse(Console.ReadLine());
+            m2 = int.Parse(Console.ReadLine());
+            d2 = int.Parse(Console.ReadLine());
+
+            if (m1 <= 6)
+                r1 = 365 - (m1 - 1) * 31 + d1;
             else
-                Console.WriteLine("no");
-            Console.ReadLine();
+                r1 = 365 - (186 + (m1 - 7) * 30 + d1);
 
+            if (m2 <= 6)
+            r2 = (m2 - 1) * 31 + d2;
+            else
 
+                r2 = 186 + (m2 - 7) * 31 + d2;
 
+            r3 = (y2 - y1 - 1)*365;
 
+            r = r1 + r2 + r3;
+            Console .WriteLine (r);
+            Console .ReadLine ();
 
-
-
-
-
-
-
-
-
-
-
+                
 
 
         }
